04dfdb9ecbab8229996b466645844152ace5d52e
Lee, Jun Seok <lego3410@gmail.com>
2020-08-02 12:07:12 +0900

    Fix bootstrap.sh not to overwrite existing files

--- keygen.py
+++ keygen.py
@@ -5,5 +5,5 @@ pool = "0123456789abcdefghijklmnopqrstuvwxyz"
 
 def keygen(length):
     return "".join([random.choice(pool) for _ in range(length)])
-print("$wgSecretKey=\t%s" % keygen(64))
-print("$wgUpgradeKey=\t%s" % keygen(16))
+print("WG_SECRETKEY=%s" % keygen(64))
+print("WG_UPGRADEKEY=%s" % keygen(16))
